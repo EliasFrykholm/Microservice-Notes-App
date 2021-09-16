95528d48729b755f1bb430a040e0709890b6bdb6
Elias Frykholm <elias.frykholm@cygni.se>
2021-09-16 14:17:18 +0200

    Add user id to jwt

--- login-service/src/main/java/com/loginservice/security/jwt/JwtUtils.java
+++ login-service/src/main/java/com/loginservice/security/jwt/JwtUtils.java
@@ -24,9 +24,10 @@ public class JwtUtils {
 
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
+                .setId(userPrincipal.getId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
     }
 
