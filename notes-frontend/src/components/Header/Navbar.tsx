957a2feb7ef5510250b264f2b8d7508deffa6dfa
Elias Frykholm <elias.frykholm@cygni.se>
2021-10-26 14:34:33 +0200

    Add searchfield to navbar

--- notes-frontend/src/components/Header/Navbar.tsx
+++ notes-frontend/src/components/Header/Navbar.tsx
@@ -5,9 +5,14 @@ import {
   Toolbar,
   Typography,
   Theme,
+  Grid,
+  InputBase,
+  alpha,
+  Box,
 } from '@material-ui/core'
-import { Menu } from '@material-ui/icons'
+import { Clear, Search } from '@material-ui/icons'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
+import { useState } from 'react'
 import { LoggedInUser } from '../../Models/User'
 
 interface NavbarProps {
@@ -23,32 +28,73 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    search: {
+      padding: '2px',
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.45),
+      },
+      maxWidth: '600px',
+      minWidth: '200px',
+      margin: 'auto',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 1),
+      width: '100%',
+    },
   }),
 )
 
 const Navbar = ({ user, onLogout }: NavbarProps) => {
   const classes = useStyles()
+  const [searchText, setSearchText] = useState('')
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <Menu />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Notes
-        </Typography>
-        {user ? (
-          <Button color="inherit" onClick={onLogout}>
-            Logout
-          </Button>
-        ) : (
-          <Button color="inherit">Login</Button>
-        )}
+        <Grid container justifyContent="space-between" spacing={2}>
+          <Grid item>
+            <Typography variant="h6" className={classes.title}>
+              Notes
+            </Typography>
+          </Grid>
+          <Grid item xs>
+            <Box className={classes.search} display="flex" flexDirection="row">
+              <IconButton size="small">
+                <Search />
+              </IconButton>
+              <InputBase
+                fullWidth
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+              {searchText && (
+                <IconButton size="small">
+                  <Clear />
+                </IconButton>
+              )}
+            </Box>
+          </Grid>
+          <Grid item>
+            {user ? (
+              <Button color="inherit" onClick={onLogout}>
+                Logout
+              </Button>
+            ) : (
+              <Button color="inherit">Login</Button>
+            )}
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   )
