2c1acbee191795139a68241428866114c043587a
Elias Frykholm <elias.frykholm@cygni.se>
2021-11-01 11:15:51 +0100

    Fix typo in endpoints

--- notes-frontend/src/API/Endpoints.ts
+++ notes-frontend/src/API/Endpoints.ts
@@ -3,5 +3,5 @@ export const SIGNUP_ENDPOINT = `${process.env.REACT_APP_LOGIN_API_URL}/signup`
 export const VALIDATE_TOKEN_ENDPOINT = `${process.env.REACT_APP_LOGIN_API_URL}/validate`
 export const REFRESH_TOKEN_ENDPOINT = `${process.env.REACT_APP_LOGIN_API_URL}/refresh`
 
-export const NOTE_ENDPOINT = `${process.env.REACT_APP_NOTE_API_URL}/note`
-export const NOTES_ENDPOINT = `${process.env.REACT_APP_NOTE_API_URL}/notes`
+export const NOTE_ENDPOINT = `${process.env.REACT_APP_NOTES_API_URL}/note`
+export const NOTES_ENDPOINT = `${process.env.REACT_APP_NOTES_API_URL}/notes`
