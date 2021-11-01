fb332739c05a65e4a273f3432404039659ffd230
Elias Frykholm <elias.frykholm@cygni.se>
2021-11-01 09:52:16 +0100

    Comment out s3 creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,25 +8,25 @@ terraform {
   required_version = "> 0.14"
 }
 
-resource "aws_s3_bucket" "keep-terraform" {
-  bucket = "keep-terraform-efrykhol"
+# resource "aws_s3_bucket" "keep-terraform" {
+#   bucket = "keep-terraform-efrykhol"
 
-  lifecycle {
-    prevent_destroy = true
-  }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
 data "aws_caller_identity" "current" {}
 
